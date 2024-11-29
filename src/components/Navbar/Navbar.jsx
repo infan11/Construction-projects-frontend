ef530bb2c85ae5bf7b7d5bb3064326ec4ac6a8a2
infan11 <infanjiounrahman20606@gmail.com>
2024-11-29 12:17:31 +0600

    added navabr

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,7 +1,66 @@
+import { NavLink } from "react-router-dom";
 
 const Navbar = () => {
+  const navLinks = <>
+   <NavLink
+  to="/"
+  className={({ isActive, isPending }) =>
+    isPending ? "pending" : isActive ? "   text-light-blue-900 hover:text-orange-900 " :  " text-light-blue-900 hover:text-orange-900"
+  }
+>
+HOME
+</NavLink>
+   <NavLink
+  to="/about"
+  className={({ isActive, isPending }) =>
+    isPending ? "pending" : isActive ? "   text-light-blue-900 hover:text-orange-900 " :  " text-light-blue-900 hover:text-orange-900"
+  }
+>
+ABOUT 
+</NavLink>
+   <NavLink
+  to="/service"
+  className={({ isActive, isPending }) =>
+    isPending ? "pending" : isActive ? "   text-light-blue-900 hover:text-orange-900 " :  " text-light-blue-900 hover:text-orange-900"
+  }
+>
+SERVICE
+</NavLink>
+   <NavLink
+  to="/allProjects"
+  className={({ isActive, isPending }) =>
+    isPending ? "pending" : isActive ? "   text-light-blue-900 hover:text-orange-900 " :  " text-light-blue-900 hover:text-orange-900"
+  }
+>
+ALL PROJECTS
+</NavLink>
+   <NavLink
+  to="/news"
+  className={({ isActive, isPending }) =>
+    isPending ? "pending" : isActive ? "   text-light-blue-900 hover:text-orange-900 " :  " text-light-blue-900 hover:text-orange-900"
+  }
+>
+NEWS
+</NavLink>
+   <NavLink
+  to="/contact"
+  className={({ isActive, isPending }) =>
+    isPending ? "pending" : isActive ? "   text-light-blue-900 hover:text-orange-900 " :  " text-light-blue-900 hover:text-orange-900"
+  }
+>
+CONTACT
+</NavLink>
+   <NavLink
+  to="/shop"
+  className={({ isActive, isPending }) =>
+    isPending ? "pending" : isActive ? "   text-light-blue-900 hover:text-orange-900 " :  " text-light-blue-900 hover:text-orange-900"
+  }
+>
+SHOP
+</NavLink>
+  </>
     return (
-        <div>
+        <div className="px-10 shadow-xl p-4">
             <div className="navbar bg-base-100">
   <div className="navbar-start">
     <div className="dropdown">
@@ -22,36 +81,18 @@ const Navbar = () => {
       <ul
         tabIndex={0}
         className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
-        <li><a>Item 1</a></li>
-        <li>
-          <a>Parent</a>
-          <ul className="p-2">
-            <li><a>Submenu 1</a></li>
-            <li><a>Submenu 2</a></li>
-          </ul>
-        </li>
-        <li><a>Item 3</a></li>
+       {navLinks}
       </ul>
     </div>
-    <a className="btn btn-ghost text-xl">daisyUI</a>
+    <a className=" text-xl"><img src={"https://i.ibb.co.com/pKSvcms/download.png"} alt="" /></a>
   </div>
-  <div className="navbar-center hidden lg:flex">
-    <ul className="menu menu-horizontal px-1">
-      <li><a>Item 1</a></li>
-      <li>
-        <details>
-          <summary>Parent</summary>
-          <ul className="p-2">
-            <li><a>Submenu 1</a></li>
-            <li><a>Submenu 2</a></li>
-          </ul>
-        </details>
-      </li>
-      <li><a>Item 3</a></li>
+
+  <div className="navbar-end">
+  <div className=" hidden lg:flex">
+    <ul className="menu menu-horizontal px-1 gap-5 font-bold text-[16px]">
+     {navLinks}
     </ul>
   </div>
-  <div className="navbar-end">
-    <a className="btn">Button</a>
   </div>
 </div>
         </div>
